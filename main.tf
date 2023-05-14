eac6588dc31dbecff697adcfecf656ed8cd65667
alfredasare <alfredasare101@gmail.com>
2023-05-14 17:46:23 +0000

    Update config

---
+++ main.tf
@@ -0,0 +1,34 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "development_vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name: "development",
+    vpc_env: "dev"
+  }
+}
+
+resource "aws_subnet" "dev-subnet-1" {
+  vpc_id = aws_vpc.development_vpc.id
+  cidr_block = "10.0.10.0/24"
+  availability_zone = "us-east-1a"
+  tags = {
+    Name: "subnet-1-dev"
+  }
+}
+
+#data "aws_vpc" "existing-vpc" {
+#  default = true
+#}
+
+#resource "aws_subnet" "dev-subnet-2" {
+#  vpc_id = data.aws_vpc.existing-vpc.id
+#  cidr_block = "172.31.96.0/20"
+#  availability_zone = "us-east-1a"
+#  tags = {
+#    Name: "subnet-2-dev"
+#  }
+#}
+
