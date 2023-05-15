90d443e0e8351a93e21667b8c0de5cdd29162b23
alfredasare <alfredasare101@gmail.com>
2023-05-15 16:53:06 +0000

    Associate igw with main route table

--- main.tf
+++ main.tf
@@ -33,8 +33,8 @@ resource "aws_internet_gateway" "myapp-igw" {
   }
 }
 
-resource "aws_route_table" "myapp-route-table" {
-  vpc_id = aws_vpc.myapp-vpc.id
+resource "aws_default_route_table" "main-rtb" {
+  default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
 
   route {
     cidr_block = "0.0.0.0/0"
@@ -42,7 +42,7 @@ resource "aws_route_table" "myapp-route-table" {
   }
 
   tags = {
-    Name: "${var.env_prefix}-rtb"
+    Name: "${var.env_prefix}-main-rtb"
   }
 }
 
