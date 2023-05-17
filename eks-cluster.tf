6fd978c317fddf47225457a59577cc3753f8157b
alfredasare <alfredasare101@gmail.com>
2023-05-17 17:21:15 +0000

    Create eks cluster with tf

---
+++ eks-cluster.tf
@@ -0,0 +1,27 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "19.13.1"
+
+  cluster_name = "myapp-eks-cluster"
+  cluster_version = "1.26"
+
+  subnet_ids = module.myapp-vpc.private_subnets
+  vpc_id = module.myapp-vpc.vpc_id
+
+  cluster_endpoint_public_access = true
+
+  tags = {
+    environment = "development"
+    application = "myapp"
+  }
+
+  eks_managed_node_groups = {
+    dev = {
+      min_size     = 1
+      max_size     = 3
+      desired_size = 3
+
+      instance_types = ["t3.micro"]
+    }
+  }
+}
