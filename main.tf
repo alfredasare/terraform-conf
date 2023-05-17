0a2bfacda502d09e00d0d1fc41d50445309effe8
alfredasare <alfredasare101@gmail.com>
2023-05-17 00:25:16 +0000

    Add modules

--- main.tf
+++ main.tf
@@ -18,108 +18,23 @@ resource "aws_vpc" "myapp-vpc" {
   }
 }
 
-resource "aws_subnet" "myapp-subnet-1" {
+module "myapp-subnet" {
+  source = "./modules/subnet"
+  subnet_cidr_block = var.subnet_cidr_block
+  avail_zone = var.avail_zone
+  env_prefix = var.env_prefix
   vpc_id = aws_vpc.myapp-vpc.id
-  cidr_block = var.subnet_cidr_block
-  availability_zone = var.avail_zone
-
-  tags = {
-    Name: "${var.env_prefix}-subnet-1"
-  }
-}
-
-resource "aws_internet_gateway" "myapp-igw" {
-  vpc_id = aws_vpc.myapp-vpc.id
-
-  tags = {
-    Name: "${var.env_prefix}-igw"
-  }
-}
-
-resource "aws_default_route_table" "main-rtb" {
   default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.myapp-igw.id
-  }
-
-  tags = {
-    Name: "${var.env_prefix}-main-rtb"
-  }
 }
 
-resource "aws_default_security_group" "myapp-default-sg" {
+module "webserver" {
+  source = "./modules/webserver"
   vpc_id = aws_vpc.myapp-vpc.id
-
-  ingress {
-    from_port = 22
-    protocol = "tcp"
-    to_port = 22
-    cidr_blocks = [var.my_ip]
-  }
-
-  ingress {
-    from_port = 8080
-    protocol  = "tcp"
-    to_port   = 8080
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port = 0
-    protocol  = "-1"
-    to_port   = 0
-    cidr_blocks = ["0.0.0.0/0"]
-    prefix_list_ids = []
-  }
-
-  tags = {
-    Name: "${var.env_prefix}-default-sg"
-  }
-}
-
-data "aws_ami" "amazon-linux-image" {
-  most_recent = true
-  owners = ["amazon"]
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-kernel-*-x86_64-gp2"]
-  }
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-}
-
-output "aws_ami_id" {
-  value = data.aws_ami.amazon-linux-image.id
-}
-
-resource "aws_instance" "myapp-server" {
-  ami = data.aws_ami.amazon-linux-image.id
+  subnet_id = module.myapp-subnet.subnet.id
+  avail_zone = var.avail_zone
+  env_prefix = var.env_prefix
   instance_type = var.instance_type
-
-  subnet_id = aws_subnet.myapp-subnet-1.id
-  vpc_security_group_ids = [aws_default_security_group.myapp-default-sg.id]
-  availability_zone = var.avail_zone
-
-  associate_public_ip_address = true
-  key_name = "ec2-server-key"
-
-  user_data = file("./entry-script.sh")
-
-  tags = {
-    Name = "${var.env_prefix}-server"
-  }
-}
-
-output "ec2_public_ip" {
-  value = aws_instance.myapp-server.public_ip
-}
-
-resource "aws_key_pair" "ssh-key" {
-  key_name = "ec2-server-key"
-  public_key = file(var.public_key_location)
+  my_ip = var.my_ip
+  public_key_location = var.public_key_location
 }
 
